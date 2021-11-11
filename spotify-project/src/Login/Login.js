412773d68b2788c7b3b4d786dff1757eaa497984
adela1806 <emyhousebnb@gmail.com>
2021-11-11 23:04:15 +0700

    push login code

---
+++ spotify-project/src/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.scss";
+function Login() {
+  return (
+    <div className="login">
+      <h1>Im the login page</h1>
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <button>LOGIN WITH SPOTIFY</button>
+    </div>
+  );
+}
+
+export default Login;
